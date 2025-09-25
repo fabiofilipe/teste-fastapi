696c0bd32183f630adfd28e32590297f93799902
fabiofilipe <fabiofmf21@gmail.com>
2025-09-25 20:55:17 -0300

    Criando banco de dados e realizando migração

--- auth_routes.py
+++ auth_routes.py
@@ -7,4 +7,9 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
 async def autenticar():
+    #comentário -> docstring
+    """
+    Essa é a rota padrão de autenticação. Todas as rotas precisam de autenticação.
+    Somente usuários autenticados podem acessar as rotas protegidas.
+    """
     return {"mensagem": "Voce acessou a rota padrão de autenticação", "autenticado": False}
