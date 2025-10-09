849cb2e7bc7954196f3ef3df183689d13a2c3b39
fabiofilipe <fabiofmf21@gmail.com>
2025-10-08 21:38:47 -0300

    estruturação e melhorias nas rotas de autenticação, login e pedido

--- auth_routes.py
+++ auth_routes.py
@@ -2,15 +2,20 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
 #auth == autenticação
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"]) 
 
+#token manual, ##ATENÇÃO melhorar isso aqui gerar um token de respeito 
+def criar_token(id_usuario):
+    token = f"sdjbsdijbsd{id_usuario}"
+    return token
+
 @auth_router.get("/")
-async def autenticar():
+async def home():
     #comentário -> docstring
     """
     Essa é a rota padrão de autenticação. Todas as rotas precisam de autenticação.
@@ -18,7 +23,7 @@ async def autenticar():
     """
     return {"mensagem": "Voce acessou a rota padrão de autenticação", "autenticado": False}
 #400 deu errado, 200 para ok
-@auth_router.post("/login")
+@auth_router.post("/criar_conta")
 #: para o fastapi entender que é um parâmetro de query
 #Depends -> injeção de dependência puxando o session da função pegar_sessao de dependencies.py
 async def login(usuario_Schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
@@ -31,5 +36,17 @@ async def login(usuario_Schema: UsuarioSchema, session: Session = Depends(pegar_
         novo_usuario = Usuario(usuario_Schema.nome, usuario_Schema.email, senha_criptografada, usuario_Schema.ativo, usuario_Schema.admin,usuario_Schema.admin )
         session.add(novo_usuario)
         session.commit()
-        return {"mensagem": f"Usuário {usuario_Schema.nome} cadastrado com sucesso!"}
+        return {"mensagem": f"Usuário {usuario_Schema.email} cadastrado com sucesso!"}
         #return {"mensagem": "Usuário cadastrado com sucesso!"}
+
+@auth_router.post("/login")    
+async def login(login_schemas:LoginSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==login_schemas.email).first()
+    #usado o query para pesquisa no banco de dados, os demais . são como consultas SQL
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não encontrado")
+    else:
+        access_token = criar_token(usuario.id)
+        return {"access_token": access_token,
+                "token_type": "Bearer"
+                }
--- order_routes.py
+++ order_routes.py
@@ -1,9 +1,20 @@
-from fastapi  import APIRouter
-
+from fastapi  import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import pegar_sessao
+from schemas import PedidoSchema
+from models import Pedido
+ 
 #order == Pedido
 
-order_router = APIRouter(prefix="/order", tags=["order"])
+order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
 @order_router.get("/")
 async def pedidos():
     return {"Voce acessou a rota de pedidos "}
+
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schemas: PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = Pedido(Usuario=pedido_schemas.id_usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {"mensagem": f"Pedido criado com sucesso. ID pedido: {novo_pedido.id}"}
